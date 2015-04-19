557cb956ad20001b48c869136174452c509d38af
scrimpist <scrimpist@projectcaspini.com>
2015-04-19 01:07:35 +0000

    Added more sample data

--- js/refund.js
+++ js/refund.js
@@ -12,6 +12,36 @@ angular.module('refundApp', [])
             "buying price": "$457",
             "current price": "$359",
             "change fee": "$50"
+        },
+        {
+            from: "SEA",
+            to: "JFK",
+            date: "05/09/2015",
+            time: "9:05 AM",
+            airline: "Alaska",
+            "buying price": "$457",
+            "current price": "$359",
+            "change fee": "$50"
+        },
+        {
+            from: "SEA",
+            to: "JFK",
+            date: "05/09/2015",
+            time: "9:05 AM",
+            airline: "Alaska",
+            "buying price": "$457",
+            "current price": "$359",
+            "change fee": "$50"
+        },
+        {
+            from: "SEA",
+            to: "JFK",
+            date: "05/09/2015",
+            time: "9:05 AM",
+            airline: "Alaska",
+            "buying price": "$457",
+            "current price": "$359",
+            "change fee": "$50"
         }
         ];
   });
