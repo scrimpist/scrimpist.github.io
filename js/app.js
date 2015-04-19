1c2afa16e2f21ba47325026c7829891e2f4a24aa
aernst <andy@weareclutch.com>
2015-04-18 19:32:54 -0700

    hook up google auth button, style tweaks

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
 'use strict';
-var lock = null;
-$(document).ready(function() {
-   lock = new Auth0Lock('HSarQ6RPNNok3mEJbyFPMKFW6vyADXDV', 'scrimpist.auth0.com');
-});
-var userProfile;
 
-$('.btn-login').click(function(e) {
-  e.preventDefault();
-  lock.show(function(err, profile, token) {
-    if (err) {
-      // Error callback
-      alert('There was an error');
-    } else {
-      // Success calback
 
-      // Save the JWT token.
-      localStorage.setItem('userToken', token);
+// $('#google-login').on('click', function(e) {
+// 	e.preventDefault();
+// 	var scope = 'email%20profile';
+// 	var redirect = 'https%3A%2F%2Fwww.scrimpist.com';
 
-      // Save the profile
-      userProfile = profile;
-      console.log(userProfile);
-    }
-  });
-});
+// 	var url = 'https://accounts.google.com/o/oauth2/auth?'+
+// 	'scope='+scope+'&'+
+// 	'redirect_uri='+redirect+'&'+
+// 	'response_type=token&'+
+// 	'client_id=276098467450-nd4dnmq8lbseb8iaggh5hdukb5f63i0k.apps.googleusercontent.com';
+// 	window.location.replace(url);
+// });
