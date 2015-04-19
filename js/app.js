df00d7caeaf9fb5bcbbb76328500d115b6cc8d77
aernst <andy@weareclutch.com>
2015-04-18 17:01:19 -0700

    add basic oauth

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 'use strict';
+var lock = null;
+$(document).ready(function() {
+   lock = new Auth0Lock('YOUR_CLIENT_ID', 'YOUR_NAMESPACE');
+});
+var userProfile;
+
+$('.btn-login').click(function(e) {
+  e.preventDefault();
+  lock.show(function(err, profile, token) {
+    if (err) {
+      // Error callback
+      alert('There was an error');
+    } else {
+      // Success calback
+
+      // Save the JWT token.
+      localStorage.setItem('userToken', token);
+
+      // Save the profile
+      userProfile = profile;
+      console.log(userProfile);
+    }
+  });
+});
